01f1aa52145f6a0efc057be0731188251cbbaa20
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-02-29 08:51:53 +0800

    Added login

--- src/main/java/com/example/mherlmanagementsystem/LoginController.java
+++ src/main/java/com/example/mherlmanagementsystem/LoginController.java
@@ -1,14 +1,26 @@
 package com.example.mherlmanagementsystem;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
 
+     Stage LoginStage;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void Login(ActionEvent event){
+        // This is the login method
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Login");
+        alert.setHeaderText("Login Successful");
+        alert.setContentText("Welcome to MHERL Management System");
+        alert.showAndWait();
+
+    }
+
+    public void setStage(Stage stage) {
+        this.LoginStage = stage;
     }
 }
--- src/main/java/com/example/mherlmanagementsystem/MherlLogin.java
+++ src/main/java/com/example/mherlmanagementsystem/MherlLogin.java
@@ -3,18 +3,26 @@ package com.example.mherlmanagementsystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MherlLogin extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MherlLogin .class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("icons/store-removebg-preview.png")));
+        stage.getIcons().add(icon);
+        stage.setTitle("Mherl Management System");
         stage.setScene(scene);
         stage.show();
+
+        LoginController controller = fxmlLoader.getController();
+        controller.setStage(stage);
+
     }
 
     public static void main(String[] args) {
