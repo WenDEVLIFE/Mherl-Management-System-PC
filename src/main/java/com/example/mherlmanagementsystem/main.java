4534183ce9626d25d7d6ed3b2595f3241749bd34
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-03-07 15:48:07 +0800

    added jar and main

---
+++ src/main/java/com/example/mherlmanagementsystem/main.java
@@ -0,0 +1,9 @@
+package com.example.mherlmanagementsystem;
+
+import javafx.application.Application;
+
+public class main {
+    public static void main(String[] args) {
+        Application.launch(MherlLogin.class, args);
+    }
+}
